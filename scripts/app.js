c715b455570638aa9dd5723d0cf8f17f8baebd1b
dannyirwin <dirwin123@gmail.com>
2021-04-28 12:23:16 -0600

    Solution

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,34 @@
 //your code here
+fetch("https://telephone-assessment.herokuapp.com/api/v1/contacts")
+    .then(response => response.json())
+    .then(json => json.data.forEach(character => {
+        addToDOM(createCard(character));
+    }));
+
+
+
+function createCard(character) {
+    const card = document.createElement("li");
+    const image = document.createElement("img");
+    const name = document.createElement("span");
+    const message = document.createElement("p");
+    const contact = document.createElement("a");
+
+    image.src = character.imageURL;
+    name.innerHTML = `${character.name} ${character.phone}`;
+    message.innerHTML = character.message;
+    contact.href = `./contact.html/character=${character.name}`
+    contact.innerHTML = `Leave ${character.name} a message`;
+    contact.target = "_blank"
+
+    card.appendChild(image);
+    card.appendChild(name);
+    card.appendChild(message);
+    card.appendChild(contact);
+
+    return card;
+}
+
+function addToDOM(element) {
+    document.querySelector("#characters").appendChild(element);
+}
